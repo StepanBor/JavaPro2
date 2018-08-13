c3d66f6c3b4bb3721ee5238fcbe09daa6f1288b2
Stepan <green7@bigmir.net>
2018-08-13 18:03:04 +0300

    s
    s

--- ContactList120818/src/main/java/com/gmail/stepan1983/Services/ContactServiceImpl.java
+++ ContactList120818/src/main/java/com/gmail/stepan1983/Services/ContactServiceImpl.java
@@ -1,14 +1,14 @@
-package ua.kiev.prog.services;
+package com.gmail.stepan1983.Services;
 
+import com.gmail.stepan1983.DAO.ContactDAO;
+import com.gmail.stepan1983.DAO.GroupDAO;
+import com.gmail.stepan1983.model.Contact;
+import com.gmail.stepan1983.model.GroupContact;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ua.kiev.prog.model.Contact;
-import ua.kiev.prog.model.Group;
-import ua.kiev.prog.dao.ContactDAO;
-import ua.kiev.prog.dao.GroupDAO;
-
 import java.util.List;
+
 
 @Service
 public class ContactServiceImpl implements ContactService {
@@ -20,32 +20,32 @@ public class ContactServiceImpl implements ContactService {
 
     @Transactional
     public void addContact(Contact contact) {
-        contactDAO.add(contact);
+        contactDAO.addContact(contact);
     }
 
     @Transactional
-    public void addGroup(Group group) {
-        groupDAO.add(group);
+    public void addGroup(GroupContact group) {
+        groupDAO.addGroup(group);
     }
 
     @Transactional
     public void deleteContact(long[] ids) {
-        contactDAO.delete(ids);
+        contactDAO.deleteContact(ids);
     }
 
     @Transactional(readOnly=true)
-    public List<Group> listGroups() {
-        return groupDAO.list();
+    public List<GroupContact> listGroups() {
+        return groupDAO.getGroups();
     }
 
     @Transactional(readOnly=true)
-    public List<Contact> listContacts(Group group, int start, int count) {
-        return contactDAO.list(group, start, count);
+    public List<Contact> listContacts(GroupContact group, int start, int count) {
+        return contactDAO.getContactList(group, start, count);
     }
 
     @Transactional(readOnly=true)
-    public List<Contact> listContacts(Group group) {
-        return contactDAO.list(group, 0, 0);
+    public List<Contact> listContacts(GroupContact group) {
+        return contactDAO.getContactList(group, 0, 0);
     }
 
     @Transactional(readOnly = true)
@@ -54,12 +54,12 @@ public class ContactServiceImpl implements ContactService {
     }
 
     @Transactional(readOnly=true)
-    public Group findGroup(long id) {
-        return groupDAO.findOne(id);
+    public GroupContact findGroup(long id) {
+        return groupDAO.getGroupById(id);
     }
 
     @Transactional(readOnly=true)
     public List<Contact> searchContacts(String pattern) {
-        return contactDAO.list(pattern);
+        return contactDAO.getContactList(pattern);
     }
 }
--- ContactList120818/src/main/java/com/gmail/stepan1983/config/AppConfig.java
+++ ContactList120818/src/main/java/com/gmail/stepan1983/config/AppConfig.java
@@ -1,12 +1,57 @@
 package com.gmail.stepan1983.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @ComponentScan("com.gmail.stepan1983")
-public class AppConfig {
+@EnableTransactionManagement
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+    @Bean
+    LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setPackagesToScan("com.gmail.stepan1983");
 
 
+        return entityManagerFactory;
+    }
+
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setPrefix("/WEB-INF/dynamic/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        resolver.setOrder(1);
+        return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("WEB-INF/static/**")
+                .addResourceLocations("WEB-INF/static/");
+    }
 
 }
